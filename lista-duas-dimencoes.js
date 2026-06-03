790539f138672344bedb41bd696ab539455a8bc7
elazarin-web <e.lazarin@escola.pr.gov.br>
2026-06-03 14:46:46 +0000

    SDAVE

--- lista-duas-dimencoes.js
+++ lista-duas-dimencoes.js
@@ -1,3 +1,5 @@
-const alunos = ["joão", "juliana", "ana", "caio"];
+const alunos = ["Juliana", "Kauã", "ana", "caio"];
 const medias = [10, 8, 7.5, 9];
-const listaDeAlunosEMedias
+const listaDeAlunosEMedias = [alunos, medias];
+
+console.log (` O aluno da posição 1 da lista de alunos é: ${listaDeAlunosEMedias[0][1]}. A nota desse aluno é ${listaDeAlunosEMedias[1][1]}`);
