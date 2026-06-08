452c2cb28e40f22964d8d358cb11018da4c132e1
elazarin-web <e.lazarin@escola.pr.gov.br>
2026-06-08 15:21:51 +0000

    SAVE

---
+++ procurando-alunos.js
@@ -0,0 +1,14 @@
+const alunos = ["juliana", "joão", "Ana", "caio"];
+const medias = [10, 8, 7.5, 9];
+const listaDeAlunosEMedias = [alunos, medias];
+function exibeNomeENota(aluno){
+    if (listaDeAlunosEMedias[0].includes(aluno)){
+        console.log(`${aluno} está cadastrado`)
+        const indice = listaDeAlunosEMedias[0].indexOf(aluno);
+        const mediaDoAluno = listaDeAlunosEMedias [1][indice];
+        console.log (mediaDoAluno);
+    }else{
+        console.log("Aluno não encontrado");
+      }
+    }
+    exibeNomeENota("Ana");
